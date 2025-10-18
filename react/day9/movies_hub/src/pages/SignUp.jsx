c5158af3e5ba2fb895b773693737e87dd562d4ea
Shreeyaa-2408 <shreeyaagrao@gmail.com>
2025-10-18 10:19:25 +0530

    day9

---
+++ react/day9/movies_hub/src/pages/SignUp.jsx
@@ -0,0 +1,14 @@
+
+// src/pages/Signup.jsx
+import SignupForm from "../components/forms/SignupFrom";
+
+
+export default function Signup() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white overflow-hidden">
+      <SignupForm />
+    </div>
+  );
+}
+
+
